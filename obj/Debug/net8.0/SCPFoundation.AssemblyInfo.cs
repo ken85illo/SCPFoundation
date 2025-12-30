388ec3899c2ed0e860a3d039e02b4b793725800d
ken85illo <pabillokenneth81@gmail.com>
2025-12-30 14:29:19 +0800

    fix: header should be fixed position

--- obj/Debug/net8.0/SCPFoundation.AssemblyInfo.cs
+++ obj/Debug/net8.0/SCPFoundation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCPFoundation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a956f006b675657efad24dafdac07ed17130cec6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a83fa7a5daa4dffff90db06d80ba1152618f52")]
 [assembly: System.Reflection.AssemblyProductAttribute("SCPFoundation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCPFoundation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
